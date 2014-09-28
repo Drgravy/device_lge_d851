367ca126f96901a3ff9107f717374c995392eb0b
Ethan Chen <intervigil@gmail.com>
2014-09-28 15:29:57 -0500

    d851: Set product identifiers

--- aicp.mk
+++ aicp.mk
@@ -10,4 +10,4 @@ $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 # Inherit device configuration
 $(call inherit-product, device/lge/d851/full_d851.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d851 BUILD_FINGERPRINT=lge/g3_tmo_us/g3:4.4.2/KVT49L.D85110c/D85110c.1403267110:user/release-keys PRIVATE_BUILD_DESC="g3_tmo_us-user 4.4.2 KVT49L.D85110c D85110c.1403267110 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE="g3" PRODUCT_NAME="g3_tmo_us" BUILD_FINGERPRINT="lge/g3_tmo_us/g3:4.4.2/KVT49L.D85110c/D85110c.1403267110:user/release-keys" PRIVATE_BUILD_DESC="g3_tmo_us-user 4.4.2 KVT49L.D85110c D85110c.1403267110 release-keys"
